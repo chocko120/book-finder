26c5d9a7af7abcd09a3aeb5e1c8bf280f31ca18f
chocko120 <chocko120@gmail.com>
2019-02-26 14:43:46 +0200

    css change for responsive

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,17 @@
 import React from 'react';
+const STRING_MAX_LENGTH = 60;
+
+const limitLength = (str) => {
+  console.log(str);
+  if (!str) return str;
+  if (str.length > STRING_MAX_LENGTH + 3){
+    return str.substring(0, STRING_MAX_LENGTH) +'...';
+  }
+  return str;
+}
+
+//Smigleckiego
+
 
 const Book = ({book}) => {
   let authors = book.volumeInfo.authors || 'Not found';
@@ -11,14 +24,14 @@ const Book = ({book}) => {
     <div className='book_wrapper'>
       <img className='book_thmb' src={thmb} alt={book.volumeInfo.title}/>
       <div className='book_content'>
-        <h2 className='book_title'>{book.volumeInfo.title}</h2>
+        <h2 className='book_title'>{limitLength(book.volumeInfo.title)}</h2>
         <h3>
           <div>By: </div>
           <div>{authors}</div>
         </h3>
         <h3>
           <div>Published by: </div>
-          <div>{book.volumeInfo.publisher || 'Unknown'}</div>
+          <div>{limitLength(book.volumeInfo.publisher) || 'Unknown'}</div>
         </h3>
         <a href={book.volumeInfo.infoLink} target='_blank' rel="noopener noreferrer"><button className='book_info_button'>See this Book</button></a>
       </div>
